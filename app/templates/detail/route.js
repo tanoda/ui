3e3f62b2db3ed08d9c68090e2fff1fbb3d38080e
Brian Wang <brian.wang19@gmail.com>
2018-06-11 10:40:09 -0700

    ayyyyy

--- app/templates/detail/route.js
+++ app/templates/detail/route.js
@@ -11,6 +11,8 @@ export default Route.extend({
       this.get('template').getOneTemplate(params.name),
       this.get('template').getTemplateTags(params.name)
     ]).then((arr) => {
+      console.log('------------PARAMS-----------')
+      console.log(params)
       const [verPayload, tagPayload] = arr;
 
       tagPayload.forEach((tagObj) => {
@@ -33,5 +35,12 @@ export default Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     controller.reset();
+  },
+  actions: {
+    willTransition(transition) {
+      console.log('in will transition at detail');
+      return true;
+      // this.modelFor('templates').reload();
+    }
   }
 });
